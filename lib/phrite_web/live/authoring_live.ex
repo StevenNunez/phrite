3c403d7dfad483a3cda5746bf75d0656c0b1cadd
Steven Nunez <steven.nunez@gmail.com>
2019-05-03 20:16:29 -0400

    Live Editting of post

--- lib/phrite_web/live/authoring_live.ex
+++ lib/phrite_web/live/authoring_live.ex
@@ -4,6 +4,7 @@ defmodule PhriteWeb.AuthoringLive do
   def render(assigns) do
     ~L"""
     <div id="authoring">
+      <textarea phx-keyup='press'></textarea>
     </div>
     <div id="preview">
       <%= @markup %>
@@ -12,6 +13,25 @@ defmodule PhriteWeb.AuthoringLive do
   end
 
   def mount(session, socket) do
-    {:ok, assign(socket, :markup, Phoenix.HTML.raw("<h1>Working</h1>"))}
+    with {:ok, markup} <- convert_to_markup("# Type something in the box") do
+      {:ok, assign(socket, :markup, markup)}
+    end
+  end
+
+  def handle_event("press", body, socket) do
+    case convert_to_markup(body) do
+      {:ok, markup} ->
+        {:noreply, assign(socket, :markup, markup)}
+
+      :error ->
+        {:noreply, socket}
+    end
+  end
+
+  defp convert_to_markup(markdown) do
+    case markdown |> Earmark.as_html() do
+      {:ok, body, []} -> {:ok, Phoenix.HTML.raw(body)}
+      {:error, _, _} -> :error
+    end
   end
 end
